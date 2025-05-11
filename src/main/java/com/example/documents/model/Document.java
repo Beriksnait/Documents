f26fa8fbad2a3452577a515e82ec0357d71df4fa
Beriks <berikslabutin@mail.ru>
2025-05-11 14:06:53 +0300

    1

--- src/main/java/com/example/documents/model/Document.java
+++ src/main/java/com/example/documents/model/Document.java
@@ -1,11 +1,10 @@
 package com.example.documents.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.Data;
 
 @Entity
+@Data
 public class Document {
 
     @Id
